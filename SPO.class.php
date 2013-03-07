7ff6ac1687d859f6e6fef96d8b25fa6779538ad8
Ramon J. A. Smit <ramon@daltcore.com>
2013-03-07 14:25:41 +0100

    Rollout Version 1.0
    Test & Demo, Please let me know how it works :)

---
+++ SPO.class.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+
+	You only need to include the class.
+	No other edits to your PHP systems!
+	Just make sure you have this class loaded as first (above anything else!)
+	
+	© Ramon Smit - Daltcore Solutions
+	SPO - SecurityPriorityOne - 2013
+
+*/
+
+class SPO{
+	
+	public function __construct(){
+		if(!empty($_POST)){
+			self::securePOST();
+		}
+		if(!empty($_GET)){
+			self::secureGET();
+		}
+		if(!empty($_COOKIE)){
+			self::secureCOOKIE();
+		}
+	}
+	
+	private function securePOST(){
+		$input_arr = array();
+		foreach ($_POST as $key => $input_arr) {
+			if(is_array($input_arr)){       
+				$_POST[$key] = self::addslashes_array($input_arr);
+			}else{
+				$_POST[$key] = self::makesave($input_arr);
+			}	
+		}
+	}
+	
+	private function secureGET(){
+		$input_arr = array();
+		foreach ($_GET as $key => $input_arr) {
+			if(is_array($input_arr)){       
+				$_GET[$key] = self::addslashes_array($input_arr);
+			}else{
+				$_GET[$key] = self::makesave($input_arr);
+			}
+			
+		}
+	}
+	
+	private function secureCOOKIE(){
+		$input_arr = array();
+		foreach ($_COOKIE as $key => $input_arr) {
+			if(is_array($input_arr)){       
+				$_COOKIE[$key] = self::addslashes_array($input_arr);
+			}else{
+				$_COOKIE[$key] = self::makesave($input_arr);
+			}   
+		}
+	}
+	
+	private function addslashes_array($input_arr){
+		if(is_array($input_arr)){
+			$tmp = array();
+			foreach ($input_arr as $key1 => $val){
+				$tmp[$key1] = self::addslashes_array($val);
+			}
+			return $tmp;
+		}else{
+			return self::makesave($input_arr);
+		}
+	}
+	
+	private function makesave($input){
+		$search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+    	$replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+		$output = str_replace($search, $replace, $input);
+		return $output;
+	}
+
+	
+}
+$SPO = new SPO;
+?>
